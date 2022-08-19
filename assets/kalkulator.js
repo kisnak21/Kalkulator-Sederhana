28e218030422e51052aef6f324fe5e4552779bda
kisnak21 <krisnastya21@gmail.com>
2022-08-19 15:25:45 +0700

    menambahkan fungsi operator sederhana javascript

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,1 +1,104 @@
-alert("selamat menggunakan kalkulator sederhana");
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false
+};
+
+function updateDisplay() {
+    document
+        .querySelector('#displayNumber')
+        .innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+        calculator.displayNumber = digit;
+    } else {
+        calculator.displayNumber += digit;
+    }
+}
+
+const buttons = document.querySelectorAll('.button');
+
+for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+
+        //mendapatkan objek elemen yang di klik
+        const target = event.target;
+
+        //mengembalikan nilai '0' jika tombol CE di klik
+        if (target.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('equals')) {
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+
+        if (target.classList.contains('operator')) {
+            handleOperator(target.innerText);
+            return;
+        }
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+
+}
+
+//fungsi positif/negatif
+function inverseNumber() {
+    if (calculator.displayNumber === '0') {
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+//fungsi handleoperator
+function handleOperator(operator) {
+    if (!calculator.isWaitForSecondNumber) {
+        calculator.operator = operator;
+        calculator.isWaitForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+
+        // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari
+        // angka pertama lagi
+        calculator.displayNumber = '0';
+    } else {
+        alert('Operator sudah ditetapkan');
+    }
+}
+
+//fungsi equals / result
+function performCalculation() {
+    if (calculator.firstNumber == null || calculator.operator == null) {
+        alert('Pilih salah satu operator!');
+        return;
+    }
+
+    let result = 0;
+    if (calculator.operator === '+') {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    } else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+
+    calculator.displayNumber = result;
+}
